b3560a6ad70665daf419bcd798e0ca30c4df7683
Breeanajp <97449904+Breeanajp@users.noreply.github.com>
2025-10-15 22:33:41 -0500

    Remove unused style in stylesheet

--- habitat-app/screens/SelectAvatarsScreen.js
+++ habitat-app/screens/SelectAvatarsScreen.js
@@ -8,18 +8,19 @@ export default function SelectAvatarsScreen({ navigation }) {
     { id: "panda", image: require("../assets/images/panda.png") }
   ];
   const handleAvatarSelect = (avatarId) => {
-        setSelected(avatarId); // Update selected avatar
+        setSelected(avatarId); //Update selected avatar
     };
 
     const handleGetStarted = () => {
         if (selectedAvatar) {
-            navigation.navigate("Home"); // Navigate to the next screen
+            navigation.navigate("Home"); //Navigate to the next screen
         }
     };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Select Your Avatar</Text>
+
         {/* View Avatars */}
         <View style={styles.avatarContainer}>
             {avatars.map((avatar) => (
@@ -31,12 +32,13 @@ export default function SelectAvatarsScreen({ navigation }) {
                     source={avatar.image}
                     style={[
                         styles.avatar,
-                        selectedAvatar === avatar.id && styles.selectedAvatar, // Highlight if selected
+                        selectedAvatar === avatar.id && styles.selectedAvatar, //Highlight if selected
                     ]}
                 />
             </TouchableOpacity>
             ))}
         </View>
+
         {/* Get started button */}
         <TouchableOpacity style={[styles.button]} onPress={handleGetStarted} disabled={!selectedAvatar}>
             <Text style={styles.buttonText}>Get Started</Text>
@@ -47,7 +49,6 @@ export default function SelectAvatarsScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: { flexGrow: 1, backgroundColor: "#fff", justifyContent: "center", padding: 20 },
   title: { fontSize: 24, fontWeight: "bold", marginBottom: 10, color: "#2d2d2d", textAlign: "center" },
-  subtitle: { fontSize: 14, color: "#666", marginBottom: 18, textAlign: "center" },
   avatarContainer: { flexDirection: "row", justifyContent: "space-around", marginBottom: 5 },
   avatar: { width: 200, height: 200, borderRadius: 45, borderWidth: 2, borderColor: "#cccbcbff" },
   selectedAvatar: { borderColor: "#4CAF50", backgroundWidth: 4 },
