2fbdcd6a39c9529c6f959af683e5ccd4b44e6b5d
hadeyemo <133528746+hadeyemo@users.noreply.github.com>
2025-10-15 21:16:38 -0500

    Rewards.js
    The rewards screen

--- habitat-app/App.js
+++ habitat-app/App.js
@@ -6,15 +6,17 @@ import { registerRootComponent } from "expo";
 import SignUpScreen from "./screens/SignUpScreen";
 import ChildProfileSetupScreen from "./screens/ChildProfileSetupScreen";
 import ChildTaskViewScreen from "./screens/ChildTaskViewScreen";
+import Rewards from "./screens/Rewards";
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ChildTaskView" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Rewards" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="ChildProfileSetup" component={ChildProfileSetupScreen} />
         <Stack.Screen name="ChildTaskView" component={ChildTaskViewScreen} />
+        <Stack.Screen name="Rewards" component={Rewards} />
       </Stack.Navigator>
     </NavigationContainer>
   );
