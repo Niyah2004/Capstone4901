1abe2674dace2b69d6d90c68ac9327d5cbe2f8c2
Janiyah Wright <90810846+Niyah2004@users.noreply.github.com>
2025-10-30 12:48:40 -0500

    remove commented code
    update remove commented code

--- habitat-app/App.js
+++ habitat-app/App.js
@@ -1,60 +1,4 @@
-/*import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { registerRootComponent } from "expo";
-import { createbottomTabNavigator }from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons"; // for icons
 
-// import any screens here so they can be used in the navigator try to keep in order of user flow
-import SignUpScreen from "./screens/SignUpScreen";
-import ChildProfileSetupScreen from "./screens/childProfileSetupScreen";
-import AvatarSelection from "./screens/AvatarSelection";
-import ChildHome from "./screens/ChildHome";
-import childTask from "./screens/childTask";
-import ChildReward from "./screens/ChildReward";
-import ParentPinScreen from "./screens/parentPinScreen";
-import ParentDashBoard from "./screens/parentDashBoard";
-import ParentTaskPage from "./screens/parentTaskPage";
-import parentReviewTask from "./screens/parentReviewTask";
-import parentReward from "./screens/parentReward";
-
-
-
-//const Stack = createNativeStackNavigator();
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="ChildProfileSetup" component={ChildProfileSetupScreen} />
-        <Stack.Screen name="ParentDashBoard" component={ParentDashBoard} />
-        <Stack.Screen name="ParentPinScreen" component={ParentPinScreen} />
-        <Stack.Screen name="ParentTaskPage" component={ParentTaskPage} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-// Register the main component
-registerRootComponent(App);
-
-export default App;
-// stack to control parent icon flow 
-//  This nested stack controls Parent icon flow
-function ParentStackScreen() {
-  return (
-    <ParentStack.Navigator screenOptions={{ headerShown: false }}>
-      <ParentStack.Screen name="ParentPinScreen" component={ParentPinScreen} />
-      <ParentStack.Screen name="ParentDashBoard" component={ParentDashBoard} />
-    </ParentStack.Navigator>
-  );
-}
-
-// for tabs navigation in bottom
-//const Tab = createBottomTabNavigator();
-// add the neccessary code for the bottom tab navigation here
-*/
  
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
@@ -74,7 +18,7 @@ import childTask from "./screens/childTask";
 import ChildReward from "./screens/ChildReward";
 import parentPinScreen from "./screens/parentPinScreen";
 import parentDashBoard from "./screens/ParentDashBoard";
-import parentTaskPage from "./screens/ParentTaskPage";
+import ParentTaskPage from "./screens/ParentTaskPage";
 import parentReviewTask from "./screens/parentReviewTask";
 import parentReward from "./screens/parentReward";
 import AccountSetting from "./screens/AccountSetting";
@@ -92,7 +36,7 @@ function ParentStackScreen() {
     <ParentStack.Navigator screenOptions={{ headerShown: false }}>
       <ParentStack.Screen name="parentPinScreen" component={parentPinScreen} />
       <ParentStack.Screen name="parentDashBoard" component={parentDashBoard} />
-      <ParentStack.Screen name="parentTaskPage" component={parentTaskPage} />
+      <ParentStack.Screen name="ParentTaskPage" component={ParentTaskPage} />
       <ParentStack.Screen name="parentReviewTask" component={parentReviewTask} />
       <ParentStack.Screen name="parentReward" component={parentReward} />
       <ParentStack.Screen name="AccountSetting" component={AccountSetting} />
