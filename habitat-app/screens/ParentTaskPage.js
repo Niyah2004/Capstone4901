ef94ed776f5dfe549fad6744f64813630ca87dbf
Breeanajp <97449904+Breeanajp@users.noreply.github.com>
2026-02-10 09:30:01 -0600

    Moved form to the top a little to match the parentReward screens

--- habitat-app/screens/ParentTaskPage.js
+++ habitat-app/screens/ParentTaskPage.js
@@ -344,7 +344,7 @@ const styles = StyleSheet.create({
   backButton: {
     alignSelf: "flex-start",
     marginLeft: 20,
-    marginTop: 40,
+    marginTop: 15,
     marginBottom: 10,
   },
   backText: {
