92b74271aa9bb54a7398691886dee0ca3865d16a
Breeanajp <97449904+Breeanajp@users.noreply.github.com>
2026-03-03 03:21:13 -0600

    Added panda wardrobe and unlock/lock logic
    Each avatar has its own wardrobe.

---
+++ habitat-app/data/avatars.js
@@ -0,0 +1,86 @@
+export const AVATARS = {
+    panda: {
+        base: require("../assets/panda.png"),
+        accessories: {
+        sleepmask: {
+            image: require("../assets/panda/accessories/sleepmask.png"),
+            cost: 10,
+            position: { top: -15, left: 42, size: 230 },
+        },
+        },
+        hats: {
+        hat: {
+            image: require("../assets/panda/hats/hat.png"),
+            cost: 10,
+            position: { top: -63.4, left: 63.9, size: 200 },
+        },
+        },
+        pants: {
+            pant: {
+                image: require("../assets/panda/pants/pant.png"),
+                cost: 20,
+                position: { top: 185, left: 83.1, size: 134.9 },
+        },
+        },
+        shoes : {
+            slippers: {
+                image: require("../assets/panda/shoes/slippers.png"),
+                cost: 15,
+                position: { top: 213, left: 82, size: 135 },
+            },
+        },
+        tops : {
+            hoodie: {  
+                image: require("../assets/panda/tops/hoodie.png"),
+                cost: 15,
+                position: { top: 137, left: 74.6, size: 148.9 },
+        },
+            top: {  
+                image: require("../assets/panda/tops/top.png"),
+                cost: 15,
+                position: { top: 130, left: 75, size: 147 },
+        }
+        },
+    },
+
+//   turtle: {
+//     base: require("../assets/turtle.png"),
+//     hats: {
+//       cap: {
+//         image: require("../assets/turtle/hats/cap.png"),
+//         cost: 5,
+//         position: { top: -25, left: 60, size: 140 },
+//       },
+//     },
+//   },
+//   dino: {
+//     base: require("../assets/dino.png"),
+//     hats: {
+//       crown: {
+//         image: require("../assets/dino/hats/crown.png"),
+//         cost: 20,
+//         position: { top: -70, left: 75, size: 180 },
+//       },
+//     },
+//   },
+//   lion: {
+//     base: require("../assets/lion.png"),
+//     hats: {
+//       crown: {
+//         image: require("../assets/lion/hats/crown.png"),
+//         cost: 20,
+//         position: { top: -70, left: 75, size: 180 },
+//       },
+//     },
+//   },
+//   penguin: {
+//     base: require("../assets/penguin.png"),
+//     hats: {
+//       cap: {
+//         image: require("../assets/penguin/hats/cap.png"),
+//         cost: 5,
+//         position: { top: -25, left: 60, size: 140 },
+//       },
+//     },
+//   },
+};
