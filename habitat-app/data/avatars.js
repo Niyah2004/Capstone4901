53991f1748dbc735a5eab5d402817fc252cad8b9
Breeanajp <97449904+Breeanajp@users.noreply.github.com>
2026-03-26 16:08:47 -0500

    Updated cost of wardrobe items for panda and dino

--- habitat-app/data/avatars.js
+++ habitat-app/data/avatars.js
@@ -4,42 +4,43 @@ export const AVATARS = {
         accessories: {
             sleepmask: {
                 image: require("../assets/panda/accessories/sleepmask.png"),
-                cost: 10,
+                cost: 0,
                 position: { top: -15, left: 42, size: 230 },
             },
         },
         hats: {
             hat: {
                 image: require("../assets/panda/hats/hat.png"),
-                cost: 10,
+                cost: 50,
                 position: { top: -63.4, left: 63.9, size: 200 },
             },
         },
-        pants: {
-            pant: {
-                image: require("../assets/panda/pants/pant.png"),
-                cost: 20,
-                position: { top: 185, left: 83.1, size: 134.9 },
-        },
-        },
-        shoes : {
-            slippers: {
-                image: require("../assets/panda/shoes/slippers.png"),
-                cost: 15,
-                position: { top: 213, left: 82, size: 135 },
-            },
-        },
+        
         tops : {
             hoodie: {  
                 image: require("../assets/panda/tops/hoodie.png"),
-                cost: 15,
+                cost: 200,
                 position: { top: 137, left: 74.6, size: 148.9 },
         },
             top: {  
                 image: require("../assets/panda/tops/top.png"),
-                cost: 15,
+                cost: 100,
                 position: { top: 130, left: 75, size: 147 },
-        }
+            }
+        },
+        pants: {
+            pant: {
+                image: require("../assets/panda/pants/pant.png"),
+                cost: 150,
+                position: { top: 185, left: 83.1, size: 134.9 },
+            },
+        },
+        shoes : {
+            slippers: {
+                image: require("../assets/panda/shoes/slippers.png"),
+                cost: 150,
+                position: { top: 213, left: 82, size: 135 },
+            },
         },
     },
 
@@ -56,31 +57,33 @@ export const AVATARS = {
     dino: {
         base: require("../assets/dino.png"),
         accessories: {
-            dog: {
-                image: require("../assets/dino/accessories/dinoDog.png"),
-                cost: 10,
-                position: { top: 190, left: 188, size: 130 },
-            },
             scarf: {
                 image: require("../assets/dino/accessories/dinoScarf.png"),
-                cost: 10,
+                cost: 0,
                 position: { top: 160, left: 97, size: 110 },
+            },
+            dog: {
+                image: require("../assets/dino/accessories/dinoDog.png"),
+                cost: 200,
+                position: { top: 190, left: 188, size: 130 },
+            },
+            
+        },
+        shoes: {
+            sneakers: {
+                image: require("../assets/dino/shoes/dinoSkates.png"),    
+                cost: 100,
+                position: { top: 213, left: 85, size: 135 },
             },
         },
         hats: {
             hat: {
                 image: require("../assets/dino/hats/dinoHat.png"),
-                cost: 20,
+                cost: 150,
                 position: { top: -50, left: 54, size: 200 },
             },
         },
-        shoes: {
-            sneakers: {
-                image: require("../assets/dino/shoes/dinoSkates.png"),    
-                cost: 15,
-                position: { top: 213, left: 85, size: 135 },
-            },
-        },
+        
    },
 //   lion: {
 //     base: require("../assets/lion.png"),
